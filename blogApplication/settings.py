99e06235883e7a0dca293638e1b69619fe947dc9
tra007 <a.alirezaloo11@gmail.com>
2023-01-26 19:56:49 +0330

    change database to postgresql

--- blogApplication/settings.py
+++ blogApplication/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'blogApplication.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "blog",
+        "USER": key.USERNAME,
+        "PASSWORD": key.PASSWORD,
     }
 }
 
